3de9b9b562d7bd91baf71a98b8733ce448034bc9
kiprono <kiprono_emmanuel@yahoo.com>
2021-04-14 17:01:24 +0300

    session

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for, session, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
 
 
 app = Flask(__name__)
 
+#config MySql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'immutable'
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+#init MYSQL
+
+mysql = MySQL(app)
+
+
+
 Articles = Articles() 
 
+#routes 
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -24,8 +43,106 @@ def articles():
 def article(id):
     return render_template('article.html', id=id)
 
+ #user registration class
 
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=5, max=20)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name= form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #create cursor 
+        cur = mysql.connection.cursor()
+         
+         #execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #commit to db 
+        mysql.connection.commit()
+
+        #close connection 
+        cur.close()
+
+        flash('you are now registered.', 'success')
+  
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+#user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+#check if user is logged in 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login','danger')
+            return redirect(url_for('login'))
+    return wrap
+
+#Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__=='__main__':
+    app.secret_key='secret123'
     app.run(debug=True)
